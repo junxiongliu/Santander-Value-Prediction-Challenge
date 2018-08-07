597fead05d559d6f1e84b00a9acf65a92268eb0a
Junxiong Liu <tomliujx@gmail.com>
2018-08-06 22:14:51 -0500

    adding lgb & svm

--- scripts/modeling/help_functions/modeling_functions.R
+++ scripts/modeling/help_functions/modeling_functions.R
@@ -17,40 +17,58 @@ CVInd <- function(n,K) { #n is sample size; K is number of parts; returns   K-le
 }
 
 ##--------------------------------------------------------------------
-## function to fit/tune knn model
-my_knn <- function(data, features, response){
-  data_fea <- data[,init_features_1]
-  data_res <- data[,response]
-  set.seed(1)
-  ctrl_knn <- trainControl(method="repeatedcv",repeats = 3, number = 10) # 10-fold cross validation
-  knnFit <- train(data_fea, data_res, method = "knn", 
-                  trControl = ctrl_knn, preProcess = c("center","scale"), tuneLength = 20)
+## function to fit/tune xgboost model (gradient boosted trees)
+my_xgb <- function(dtrain, depth = 10000, lr = 0.01, es = 10){ # chang depth and lr if needed
+  # input the xgb.Dmatrix format dtrain
   
-  return (knnFit)
-}
-
-##--------------------------------------------------------------------
-## function to fit/tune lm-stepwise model
-my_lmstep <- function(data, features, response){
-  data_fea <- data[,features]
-  data_res <- data[,response]
-  set.seed(1)
-  ctrl_lm <- trainControl(method="repeatedcv",repeats = 3, number = 10) # 10-fold cross validation
-  lmFit <- train(data_fea, data_res, method = "leapSeq", 
-                 trControl = ctrl_lm, tuneLength = 20)
+  # initialize
+  maxTrees <- depth
+  shrinkage <- lr
+  gamma <- 2
+  depth <- 10
+  minChildWeight <- 20
+  colSample <- 0.9
+  subSample <- 0.9
+  earlyStopRound <- es
   
-  return (lmFit)
-}
-
-##--------------------------------------------------------------------
-## function to fit/tune ridge regression model
-my_ridge <- function(data, features, response){
-  data_fea <- data[,features]
-  data_res <- data[,response]
+  # tune the tree
   set.seed(1)
-  ctrl_ridge <- trainControl(method="repeatedcv",repeats = 3, number = 10) # 10-fold cross validation
-  ridgeFit <- train(data_fea, data_res, method = "ridge", 
-                    trControl = ctrl_ridge, tuneLength = 10)
+  xgbCV <- xgb.cv(params=list(max_depth=depth,
+                              eta=shrinkage,
+                              gamma=gamma,
+                              colsample_bytree=colSample,
+                              min_child_weight=minChildWeight,
+                              subsample=subSample,
+                              objective="reg:linear",
+                              silent=0),
+                  data=dtrain,
+                  nrounds=maxTrees,
+                  # verobse=0,
+                  eval_metric="rmse",
+                  nfold=10,
+                  stratified=TRUE,
+                  early_stopping_rounds=earlyStopRound)
+  
+  bestIter <- xgbCV$best_iteration
+  
+  # get the model
+  xgb_best <- xgboost(params=list(max_depth=depth,
+                                  eta=shrinkage,
+                                  gamma=gamma,
+                                  colsample_bytree=colSample,
+                                  min_child_weight=minChildWeight,
+                                  subsample=subSample,
+                                  objective="reg:linear"),
+                      data=dtrain,
+                      nrounds=bestIter,
+                      verbose=0,
+                      eval_metric ="rmse")
+  
+  print ("-------------------------------------------------------")
+  print ("xgb this round complete!")
+  print ("-------------------------------------------------------")
+  
+  return (xgb_best)
 }
 
 ##--------------------------------------------------------------------
@@ -125,56 +143,82 @@ my_rf <- function(data, response){
 }
 
 ##--------------------------------------------------------------------
-## function to fit/tune xgboost model (gradient boosted trees)
-my_xgb <- function(dtrain, depth = 10000, lr = 0.002, es = 10){ # chang depth and lr if needed
-  # input the xgb.Dmatrix format dtrain
-  
-  # initialize
-  maxTrees <- depth
-  shrinkage <- lr
-  gamma <- 2
-  depth <- 10
-  minChildWeight <- 20
-  colSample <- 0.9
-  subSample <- 0.9
-  earlyStopRound <- es
-  
-  # tune the tree
+## function to fit/tune knn model
+my_knn <- function(data, features, response){
+  data_fea <- data[,init_features_1]
+  data_res <- data[,response]
   set.seed(1)
-  xgbCV <- xgb.cv(params=list(max_depth=depth,
-                              eta=shrinkage,
-                              gamma=gamma,
-                              colsample_bytree=colSample,
-                              min_child_weight=minChildWeight,
-                              subsample=subSample,
-                              objective="reg:linear",
-                              silent=0),
-                  data=dtrain,
-                  nrounds=maxTrees,
-                  # verobse=0,
-                  eval_metric="rmse",
-                  nfold=10,
-                  stratified=TRUE,
-                  early_stopping_rounds=earlyStopRound)
+  ctrl_knn <- trainControl(method="repeatedcv",repeats = 3, number = 10) # 10-fold cross validation
+  knnFit <- train(data_fea, data_res, method = "knn", 
+                  trControl = ctrl_knn, preProcess = c("center","scale"), tuneLength = 20)
   
-  bestIter <- xgbCV$best_iteration
-  
-  # get the model
-  xgb_best <- xgboost(params=list(max_depth=depth,
-                             eta=shrinkage,
-                             gamma=gamma,
-                             colsample_bytree=colSample,
-                             min_child_weight=minChildWeight,
-                             subsample=subSample,
-                             objective="reg:linear"),
-                 data=dtrain,
-                 nrounds=bestIter,
-                 verbose=0,
-                 eval_metric ="rmse")
-  
-  print ("-------------------------------------------------------")
-  print ("xgb this round complete!")
-  print ("-------------------------------------------------------")
-  
-  return (xgb_best)
+  return (knnFit)
 }
+
+##--------------------------------------------------------------------
+## function to fit/tune lm-stepwise model
+my_lmstep <- function(data, features, response){
+  data_fea <- data[,features]
+  data_res <- data[,response]
+  set.seed(1)
+  ctrl_lm <- trainControl(method="repeatedcv",repeats = 3, number = 10) # 10-fold cross validation
+  lmFit <- train(data_fea, data_res, method = "leapSeq", 
+                 trControl = ctrl_lm, tuneLength = 20)
+  
+  return (lmFit)
+}
+
+##--------------------------------------------------------------------
+## function to fit/tune ridge regression model
+my_ridge <- function(data, features, response){
+  data_fea <- data[,features]
+  data_res <- data[,response]
+  set.seed(1)
+  ctrl_ridge <- trainControl(method="repeatedcv",repeats = 3, number = 10) # 10-fold cross validation
+  ridgeFit <- train(data_fea, data_res, method = "ridge", 
+                    trControl = ctrl_ridge, tuneLength = 10)
+}
+
+##--------------------------------------------------------------------
+## function to fit/tune SVM model (Linear Kernel)
+my_svmLinear <- function(data, features, response){
+  data_fea <- data[,init_features_1]
+  data_res <- data[,response]
+  set.seed(1)
+  ctrl_svm <- trainControl(method="repeatedcv",repeats = 3, number = 10) # 10-fold cross validation
+  svmFit <- train(data_fea, data_res, method = "svmLinear", 
+                  trControl = ctrl_svm, tuneLength = 20)
+  
+  return (svmFit)
+}
+
+##--------------------------------------------------------------------
+## function to fit/tune lightgbm
+my_lgb <- function(dtrain, depth = 10000, lr = 0.01, es = 10){
+  # tune first
+  params <- list(objective = "regression", metric = "rmse")
+  lgb_cv <- lgb.cv(params, 
+                   data = dtrain,
+                   nrounds = depth, learning_rate = lr, nfold = 10,
+                   boosting = "gbdt", max_depth = 10, feature_fraction = 0.9, bagging_fraction = 0.8,
+                   early_stopping_rounds = es,
+                   num_threads = 4)
+  lgb_best_iter <- lgb_cv$best_iter
+  
+  ## fit 
+  lgb_best <- lgb.train(params, 
+                        data = dtrain,
+                        nrounds = lgb_best_iter, learning_rate = lr, nfold = 10,
+                        boosting = "gbdt", num_threads = 4)
+  
+  return (lgb_best)
+  
+}
+
+# -----------------------------------------------------------
+# Neural Network
+### TO BE FILLED HERE
+
+##--------------------------------------------------------------------
+## function to fit/tune neural network
+
